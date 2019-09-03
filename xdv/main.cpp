ecf5a57c4c45fc6f4533e7395d1218932f261153
0a777h <0a777h@gmail.com>
2019-09-03 16:26:42 +0900

    xdv:: Fixed build references

--- xdv/main.cpp
+++ xdv/main.cpp
@@ -10,6 +10,7 @@
 #include "xdv_sdk.h"
 #pragma comment(lib, "corexts.lib")
 
+#if 0
 int main(int argc, char * argv[])
 {
 	if (argc < 2)
@@ -38,3 +39,26 @@ int main(int argc, char * argv[])
 
 	return 0;
 }
+#else
+int __stdcall WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nShowCmd
+)
+{
+	XdvLoadModule("qt\\Qt5Core");
+	XdvLoadModule("qt\\Qt5Gui");
+	XdvLoadModule("qt\\Qt5Widgets");
+	XdvLoadModule("qt\\qwindows");
+	if (XdvLoadModule("qxnm"))
+	{
+		ShowWindow(GetConsoleWindow(), SW_HIDE);
+		return (int)ullvar(XdvExe("!qxnm.xenom"));
+	}
+	else
+	{
+		printf("> not found qxnm\n"), _getch();
+	}
+}
+#endif
